8163ebd0ae89a2e172a4d3de05667886fe6e1add
Nawab_AS <hamid.syed.m@gmail.com>
2025-01-31 17:07:41 -0500

    first commit

---
+++ __socket_server__.js
@@ -0,0 +1,42 @@
+let express = require("express");
+var app = require("express")();
+var http = require("http").createServer(app);
+let io = require("socket.io")(http);
+let fs = require("fs");
+const PASSWORD = "test1";
+let file = ""
+
+app.use(express.static(__dirname + "/public"));
+app.use(function (req, res) {
+    url = new URL("http://localhost:80"+req.url)
+    if (fs.existsSync(__dirname + "/public" + url.pathname)){
+    res.sendFile(__dirname + "/public" + req.url);
+    } else {
+        res.sendFile(__dirname + "/public/404.html");
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+    let auth = false;
+
+    socket.on("auth", (data) => {
+        if (data.password == PASSWORD){
+            auth = true;
+            io.emit("edit", {text:file, id:"null"});
+        } else {
+            socket.disconnect();
+        }
+    });
+
+    socket.on("sync", (data)=>{
+        if (auth){
+            io.emit("edit", {text:data.text, id:data.id});
+            file = data.text;
+        }
+    });
+});
+
+http.listen(80, function () {
+    console.log("http listening on *:80");
+});
