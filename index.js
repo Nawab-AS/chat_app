586aa40eca1e6d18f0cfe7de8bd422330a514317
Hamid Syed <59747708+Nawab-AS@users.noreply.github.com>
2025-02-25 00:18:25 +0000

    connected to replit

--- index.js
+++ index.js
@@ -5,21 +5,43 @@ let io = require("socket.io")(http);
 let fs = require("fs");
 const PASSWORD = process.env.PASSWORD;
 let file = "";
-
 if (PASSWORD == undefined) {
     console.log("ERROR: No password set");
 }
 
+
+// routes
 app.use(express.static(__dirname + "/public"));
-app.use(function (req, res) {
-    url = new URL("http://localhost:80"+req.url)
-    if (fs.existsSync(__dirname + "/public" + url.pathname)){
-    res.sendFile(__dirname + "/public" + req.url);
-    } else {
-        res.sendFile(__dirname + "/public/404.html");
-    }
+
+app.get("/", (req, res) => { // home page
+    res.sendFile(__dirname + "/public/home/index.html");
 });
 
+function circularReplacer() {
+  const seen = new WeakSet(); // object
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+}
+
+app.use(function (req, res) { // other files
+    
+    res.json(JSON.parse(JSON.stringify(obj, circularReplacer())));
+    /*if (fs.existsSync(__dirname + "/public" + req.url)){
+        res.sendFile(__dirname + "/public" + req.url); // send file if path exists
+    } else {
+        res.sendFile(__dirname + "/public/404.html"); // send 404 if path doesn't exist
+    }*/
+});
+
+
+// socket.io
 io.on("connection", (socket) => {
     console.log("User connected");
     let auth = false;
